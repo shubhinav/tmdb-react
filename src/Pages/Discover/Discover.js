a89cd1ac1bfdcd0de07de81af8fe545c84f01540
Shubhinav Dua <shubhinavdua@gmail.com>
2023-01-01 23:07:59 -0800

    added coming soon message on discover page

--- src/Pages/Discover/Discover.js
+++ src/Pages/Discover/Discover.js
@@ -1,9 +1,15 @@
 import Header from "../../Components/Header/Header";
+import FadeIn from "react-fade-in/lib/FadeIn";
 
-export default function Discover(){
-    return(
+export default function Discover() {
+    return (
         <div>
-            <Header page='discover'/>
+            <Header page='discover' />
+            <FadeIn>
+                <h1 className="content-container mt-5 text-center">
+                    Coming Soon!
+                </h1>
+            </FadeIn>
         </div>
     )
 }
