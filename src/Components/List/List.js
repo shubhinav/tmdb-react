9ef10e8352a5c09f699e4242c58c59753440efaf
Shubhinav Dua <shubhinavdua@gmail.com>
2023-01-27 14:11:31 -0800

    drag scroll on list

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,23 +1,26 @@
 import './list.css'
 import Card from '../Card/Card'
+import ScrollContainer from 'react-indiana-drag-scroll'
+import { useRef } from 'react'
 
-export default function List({title, data}){
-    return(
-        <div className='content-container' style={{marginTop: '1.75em'}}>
+export default function List({ title, data }) {
+    return (
+
+        <div className='content-container' style={{ marginTop: '1.75em' }}>
             <h1 className='list-title'>{title}</h1>
-            <div className='list-content d-flex overflow-auto'>
-                {data.map((ent, i)=>{
+            <ScrollContainer className='list-content d-flex overflow-auto'>
+                {data.map((ent, i) => {
                     return <Card key={i}
-                                 title={ent.title} 
-                                 name={ent.name} 
-                                 character={ent.character} 
-                                 job={ent.job} 
-                                 movieImgUrl={ent.poster_path} 
-                                 castImgUrl={ent.profile_path} 
-                                 vidKey={ent.key}
-                                 id={ent.id}/>
+                        title={ent.title}
+                        name={ent.name}
+                        character={ent.character}
+                        job={ent.job}
+                        movieImgUrl={ent.poster_path}
+                        castImgUrl={ent.profile_path}
+                        vidKey={ent.key}
+                        id={ent.id} />
                 })}
-            </div>
+            </ScrollContainer>
         </div>
     )
 }
